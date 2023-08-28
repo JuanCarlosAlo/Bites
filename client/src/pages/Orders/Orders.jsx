7a20e3621c7ac568f74592e8a4fa481e04730465
JC <jc.alonsome@gmail.com>
2023-08-28 17:49:53 +0200

    orders empty fix

--- client/src/pages/Orders/Orders.jsx
+++ client/src/pages/Orders/Orders.jsx
@@ -8,6 +8,9 @@ import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/Auth.context';
 import Secondaryheader from '../../components/secondary-header/SecondaryHeader';
+import Text from '../../components/text/Text';
+import { MEASUREMENTS } from '../../constants/measurements';
+import { COLORS } from '../../constants/colors';
 
 const Orders = () => {
 	const { currentUser, loadingFirebase } = useContext(AuthContext);
@@ -21,13 +24,22 @@ const Orders = () => {
 	return (
 		<PageComponent isBack={true}>
 			<Secondaryheader url={'/'} />
-			{data.orders.map(order => (
-				<OrderContainer
-					key={order._id}
-					order={order}
-					currentUser={currentUser}
+			{data.lenght === 0 ? (
+				<Text
+					align={MEASUREMENTS.ALIGN.CENTER}
+					color={COLORS.MAIN}
+					fontSize={MEASUREMENTS.FONTS_SIZE.KEY.TITLE}
+					text={`You haven't made any orders yet`}
 				/>
-			))}
+			) : (
+				data.orders.map(order => (
+					<OrderContainer
+						key={order._id}
+						order={order}
+						currentUser={currentUser}
+					/>
+				))
+			)}
 		</PageComponent>
 	);
 };
