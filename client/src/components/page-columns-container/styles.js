ede8dcb343ac38f65839f793ae457ed28ba5b1b0
JC <jc.alonsome@gmail.com>
2023-08-31 19:38:17 +0200

    colums

--- client/src/components/page-columns-container/styles.js
+++ client/src/components/page-columns-container/styles.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 const StyledPageColumnsContainer = styled.div`
 display: grid;
-grid-template-columns: repeat(2, 1fr);
+grid-template-columns: repeat(1, 1fr);
 grid-template-rows: repeat(auto-fill, 250px);
 gap: 1rem;
 height: 100%;
 width: 100%;
 padding-left: 10px;
 overflow-y: scroll;
-@media screen and (min-width: 1024px) {
+@media screen and (min-width: 768px) {
     grid-template-columns: repeat(3, 1fr); 
 }
 `
