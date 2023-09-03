0891abcca599c397ade353baf37e2902f29b9b3b
JC <jc.alonsome@gmail.com>
2023-09-03 02:29:29 +0200

    colums mobile fixed

--- client/src/components/page-columns-container/styles.js
+++ client/src/components/page-columns-container/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledPageColumnsContainer = styled.div`
 display: grid;
-grid-template-columns: repeat(2, 1fr);
+grid-template-columns: repeat(1, 1fr);
 grid-template-rows: repeat(auto-fill, 250px);
 gap: 1rem;
 height: 100%;
