93a47444222af7844fecca45a6fd50189ebdbb14
JC <jc.alonsome@gmail.com>
2023-10-09 11:18:49 +0200

    cart button icon

--- client/src/components/cart-button/CartButton.jsx
+++ client/src/components/cart-button/CartButton.jsx
@@ -11,12 +11,13 @@ const CartButton = () => {
 	const navigate = useNavigate();
 	return (
 		<StyledCartButton onClick={() => navigate('/cart')}>
-			<Text
+			{/* <Text
 				align={MEASUREMENTS.ALIGN.CENTER}
 				color={COLORS.WHITE}
 				fontSize={MEASUREMENTS.FONTS_SIZE.KEY.SUBTITLE}
 				text={`Cart ${cartItems.length}`}
-			/>
+			/> */}
+			<Icon img={'/images/add/to/cart.svg'} />
 		</StyledCartButton>
 	);
 };
